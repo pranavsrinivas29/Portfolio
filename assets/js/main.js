01bbd3665f9c2e100627614407e31732633d742e
pranavsrinivas29 <57083794+pranavsrinivas29@users.noreply.github.com>
2024-05-14 11:58:36 +0200

    First Commit before deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,97 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+function showProjectSection() {
+    $('.project-section').each(function(index) {
+        var projectSection = $(this);
+        setTimeout(function() {
+            projectSection.addClass('show');
+        }, 200 * index);
+    });
+}
+
+$(document).ready(function() {
+    showProjectSection();
+});
+
+$(document).ready(function() {
+    // Show "Read More" button if description is truncated
+    $('.project-details').each(function() {
+        var projectDetails = $(this);
+        var description = projectDetails.find('.description');
+        if (description[0].scrollHeight > description.innerHeight()) {
+            projectDetails.find('.read-more-btn').show();
+        }
+    });
+
+    // Toggle between full and truncated description on "Read More" click
+    $('.read-more-btn').click(function(e) {
+        e.preventDefault();
+        var projectDetails = $(this).closest('.project-details');
+        var description = projectDetails.find('.description');
+        var readMoreBtn = projectDetails.find('.read-more-btn');
+        var readLessBtn = projectDetails.find('.read-less-btn');
+
+        description.css('max-height', 'none');
+        readMoreBtn.hide();
+        readLessBtn.show();
+    });
+
+    // Toggle between full and truncated description on "Read Less" click
+    $('.read-less-btn').click(function(e) {
+        e.preventDefault();
+        var projectDetails = $(this).closest('.project-details');
+        var description = projectDetails.find('.description');
+        var readMoreBtn = projectDetails.find('.read-more-btn');
+        var readLessBtn = projectDetails.find('.read-less-btn');
+
+        description.css('max-height', 'calc(3 * 1.2em)'); // Adjust this value to display three lines
+        readLessBtn.hide();
+        readMoreBtn.show();
+    });
+});
+
+
+$(document).ready(function() {
+    // Toggle between full and truncated description on "Read More" click
+    $('.read-more-btn').click(function(e) {
+        e.preventDefault();
+        var projectDetails = $(this).closest('.project-details');
+        var description = projectDetails.find('.description');
+
+        // Toggle between truncated and full description
+        if (description.hasClass('truncated')) {
+            description.removeClass('truncated').css('max-height', 'none');
+            $(this).hide();
+            projectDetails.find('.read-less-btn').show();
+        } else {
+            description.addClass('truncated').css('max-height', 'calc(3 * 1.2em)'); // Adjust this value to display three lines
+            $(this).show();
+            projectDetails.find('.read-less-btn').hide();
+        }
+    });
+
+
+
+
+	// Toggle between full and truncated description on "Read Less" click
+    $('.read-less-btn').click(function(e) {
+        e.preventDefault();
+        var projectDetails = $(this).closest('.project-details');
+        var description = projectDetails.find('.description');
+        var readMoreBtn = projectDetails.find('.read-more-btn');
+        var readLessBtn = projectDetails.find('.read-less-btn');
+
+        // Toggle between truncated and full description
+        if (description.hasClass('truncated')) {
+            description.removeClass('truncated').css('max-height', 'none');
+            readMoreBtn.hide();
+            readLessBtn.show();
+        } else {
+            description.addClass('truncated').css('max-height', 'calc(3 * 1.2em)'); // Adjust this value to display three lines
+            readMoreBtn.show();
+            readLessBtn.hide();
+        }
+    });
+});
